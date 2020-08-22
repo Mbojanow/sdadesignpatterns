57f9c51a7c46ff2a60eb29795d378ae78f8ee77f
Michal Bojanowski <michal.bojanowski@onegini.com>
2020-08-22 13:27:41 +0200

    WIP2

--- src/main/java/pl/sdacademy/designpatterns/Test.java
+++ src/main/java/pl/sdacademy/designpatterns/Test.java
@@ -1,7 +1,35 @@
 package pl.sdacademy.designpatterns;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Test {
   public static void main(String[] args) {
-    System.out.println("Ok project imported correctly");
+    List<IntWrapper> elements = new ArrayList<>();
+    elements.add(new IntWrapper());
+    elements.add(new IntWrapper());
+
+    List<IntWrapper> shallowCopyElements = elements; // shallow copy
+    shallowCopyElements.add(new IntWrapper());
+
+    System.out.println(elements.size());
+
+    List<IntWrapper> deepCopyElements = new ArrayList<>(elements); // deep copy
+    deepCopyElements.add(new IntWrapper());
+
+    System.out.println(elements.size());
+
+  }
+}
+
+class IntWrapper {
+  private int value;
+
+  public int getValue() {
+    return value;
+  }
+
+  public void setValue(final int value) {
+    this.value = value;
   }
 }
